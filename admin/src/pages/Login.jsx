bcb023fcb67c1387fdc61b74882d008f3036b951
AdityaGowala <adityagowala656@gmail.com>
2025-09-29 01:50:43 +0530

    error 4

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useContext, useState } from 'react'
-import useNavigate from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { DoctorContext } from '../context/DoctorContext'
 import { AdminContext } from '../context/AdminContext'
 import { toast } from 'react-toastify'
